d3648696734b6faf0d9997ef72c7f6c848acb6d9
ggottemo <ggottemo717@gmail.com>
2022-12-01 17:12:53 -0500

    thank you node

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/',
-    hashFunction: "sha256",
+    hashFunction: "xxhash64",
   },
   module: {
     rules: [
